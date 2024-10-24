2a07523d592590bb924203f88a9afc4dfd8e5f38
Vida Zarei <vida1997zarei@gmail.com>
2024-10-24 14:59:00 -0400

    ProductRequestDto was coded

--- OnlineShop/Models/Product/ProductRequestDto.cs
+++ OnlineShop/Models/Product/ProductRequestDto.cs
@@ -1,7 +1,21 @@
-﻿namespace OnlineShop.Models.Product
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models.Product
 {
     public class ProductRequestDto
     {
+        [Required(ErrorMessage = "Name must be filled")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Price must be filled")]
+        public string Price { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsActive { get; set; }
+        [Required(ErrorMessage = "Please enter Category Id")]
+        public int CategoryId { get; set; }
+        // Navigation Property
         
     }
 }
