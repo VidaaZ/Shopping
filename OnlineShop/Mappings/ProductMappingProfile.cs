7e82dc0400c7f654f16e5b3eaffb5009abda8762
Vida Zarei <vida1997zarei@gmail.com>
2024-11-13 14:55:45 -0500

    ProfileMappingProfile was coded.

--- OnlineShop/Mappings/ProductMappingProfile.cs
+++ OnlineShop/Mappings/ProductMappingProfile.cs
@@ -1,6 +1,19 @@
-﻿namespace OnlineShop.Mappings
+﻿using AutoMapper;
+using OnlineShop.entities;
+using OnlineShop.Models.Product;
+
+namespace OnlineShop.Mappings
 {
-    public class ProductMappingProfile
-    {
+   
+        public class ProductMappingProfile : Profile
+        {
+            public ProductMappingProfile()
+            {
+                // Define your mappings here
+                CreateMap<Product, ProductResponseDto>();
+                CreateMap<ProductRequestDto, Product>();
+                CreateMap<UpdateProductRequestDto, Product>();
+            }
+        }
     }
-}
+
