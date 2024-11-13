ab8773dd06387a57b4c34d0925905d292ef8a627
Vida Zarei <vida1997zarei@gmail.com>
2024-11-13 18:55:11 -0500

    SignUp Entity was created.

---
+++ OnlineShop/entities/SignUp.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.entities
+{
+    public class SignUp
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        
+        public string Address { get; set; }
+        [Required]
+        public String PasswordHash { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
