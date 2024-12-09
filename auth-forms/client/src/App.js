8c8cd3c371044ddba39503a6e850c2a822fb1d77
Vida Zarei <vida1997zarei@gmail.com>
2024-12-09 08:10:19 -0500

    signup and login has been finalized

--- auth-forms/client/src/App.js
+++ auth-forms/client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUpForm from './components/SignupForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+
+    return (
+        <span>
+            hello
+        </span>
+    );
+    //return (
+    //    <Router>
+    //        <Routes>
+    //            {/* Define a route for the signup form */}
+    //            <Route path="/" element={<h1>Welcome to Onlineshop</h1>} />
+    //            <Route path="/signup" element={<SignUpForm />} />
+    //        </Routes>
+    //    </Router>
+    //);
+};
 export default App;
