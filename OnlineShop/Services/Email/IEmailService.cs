9720f3fad2da009a82e4ece55a4006cdcbf39e95
Vida Zarei <vida1997zarei@gmail.com>
2025-01-05 11:59:28 -0500

    |Send Email API

---
+++ OnlineShop/Services/Email/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using OnlineShop.Models.Email;
+
+namespace OnlineShop.Services.Email
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailRequestDto emailRequest);
+    }
+}
